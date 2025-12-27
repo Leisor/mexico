915b2ea07d3892f98d42db9585fb64b34e6265cf
Heikki Leskelä <hleskel@gmail.com>
2025-12-27 06:24:25 +0000

    Player and HumanPlayer classes added for future implementation

---
+++ src/main/java/io/github/leisor/HumanPlayer.java
@@ -0,0 +1,37 @@
+package io.github.leisor;
+
+import org.jline.terminal.Terminal;
+
+public class HumanPlayer extends Player {
+    public HumanPlayer(int playerNumber, int lives) {
+        super(playerNumber, lives);
+    }
+
+    @Override
+    public boolean decideToRoll(int rollNumber, Terminal terminal) throws Exception {
+        if (rollNumber == 0) {
+            System.out.println("Press R to roll the dice.");
+        } else {
+            System.out.println("Press R to roll again or S to stay.");
+        }
+
+        while (true) {
+            int input = terminal.reader().read();
+            char key = (char) input;
+            String keyStr = String.valueOf(key).toLowerCase();
+
+            if (keyStr.equals("r")) {
+                return true;
+            } else if (keyStr.equals("s") && rollNumber > 0) {
+                return false;
+            } else {
+                System.out.println("Invalid input. Try again.");
+            }
+        }
+    }
+
+    @Override
+    public String getPlayerType() {
+        return "Human";
+    }
+}
---
+++ src/main/java/io/github/leisor/Player.java
@@ -0,0 +1,48 @@
+package io.github.leisor;
+
+import org.jline.terminal.Terminal;
+
+public abstract class Player {
+    protected int playerNumber;
+    protected int lives;
+    protected int[] lastRoll;
+    public Player(int playerNumber, int lives) {
+        this.playerNumber = playerNumber;
+        this.lives = lives;
+        this.lastRoll = null;
+    }
+
+    public abstract boolean decideToRoll(int rollNumber, Terminal terminal) throws Exception;
+
+    public abstract String getPlayerType();
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void loseLife() {
+        if (lives > 0) {
+            lives--;
+        }
+    }
+
+    public boolean isAlive() {
+        return lives > 0;
+    }
+
+    public void setLastRoll(int[] roll) {
+        this.lastRoll = roll;
+    }
+
+    public int[] getLastRoll() {
+        return lastRoll;
+    }
+
+
+
+
+}
